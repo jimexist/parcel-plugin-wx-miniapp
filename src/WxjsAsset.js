7df8874e58bf2bea162cb2f09a4a5eec5e9127a4
Jiayu Liu <jiayu.liu@airbnb.com>
2018-07-08 11:49:50 +0800

    update logger

--- src/WxjsAsset.js
+++ src/WxjsAsset.js
@@ -6,20 +6,22 @@ export default class WxjsAsset extends Asset {
   constructor(name, options) {
     super(name, options);
     this.type = "js";
-    logger.log(`constructor ${{ name, options }}`);
+    logger.log(
+      `constructor, name: ${name}, options: ${JSON.stringify(options)}`
+    );
     this.delegate = new JsAsset(name, options);
   }
 
   async parse(code) {
-    logger.log(`parse ${{ code }}`);
+    logger.log(`parse, code: ${code}`);
     const result = await this.delegate.parse(code);
-    logger.log(`parse ${{ result }}`);
+    logger.log(`parse, result: ${result}`);
     return result;
   }
 
   async generate() {
     const result = await this.delegate.generate();
-    logger.log(`generate ${{ result }}`);
+    logger.log(`generate, result: ${result}`);
     return result;
   }
 }
--- src/WxssAsset.js
+++ src/WxssAsset.js
@@ -6,20 +6,22 @@ export default class WxssAsset extends Asset {
   constructor(name, options) {
     super(name, options);
     this.type = "wxss";
-    logger.log(`constructor ${{ name, options }}`);
+    logger.log(
+      `constructor, name: ${name}, options: ${JSON.stringify(options)}`
+    );
     this.delegate = new CSSAsset(name, options);
   }
 
   async parse(code) {
-    logger.log(`parse ${{ code }}`);
+    logger.log(`parse, code: ${code}`);
     const result = await this.delegate.parse(code);
-    logger.log(`parse ${{ result }}`);
+    logger.log(`parse, result: ${result}`);
     return result;
   }
 
   async generate() {
     const result = await this.delegate.generate();
-    logger.log(`generate ${{ result }}`);
+    logger.log(`generate, result: ${result}`);
     return result;
   }
 }
